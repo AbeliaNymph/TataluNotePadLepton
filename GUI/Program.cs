9fe2fba42be487a59b3eda276fbcca834ffc8683
Abelia Nymph <Abelianymph@outlook.com>
2020-11-10 21:27:38 +0800

    项目初始化

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -13,7 +13,7 @@ namespace GUI
 			//Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			//Application.EnableVisualStyles();
 			//Application.SetCompatibleTextRenderingDefault(false);
-			//Application.Run(new BaseForm());
+			//Application.Run(new BaseForm()); 
 		}
 	}
 }
